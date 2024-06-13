a42ed9f5a9dfd4f9fe31391ce6d50d4f66a1d48f
Elsabordeaux <snowpaws85@gmail.com>
2024-06-13 09:39:11 +0100

    Added basic js form handling and added axios and typewriter plugin

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function generateInformation(event) {
+  event.preventDefault();
 
+  new Typewriter("#generated-text", {
+    strings: "Hello",
+    autoStart: true,
+    delay: 50,
+  });
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", generateInformation);
