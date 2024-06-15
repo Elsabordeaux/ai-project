0a5df94d2cd9dd7a47241153703c18d3d58c1e91
Elsabordeaux <snowpaws85@gmail.com>
2024-06-15 17:06:03 +0100

    Added AI API call

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+function displayInfo(response) {
+  new Typewriter("#generated-text", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 70,
+    cursor: "",
+  });
+}
+
 function generateInformation(event) {
   event.preventDefault();
 
-  new Typewriter("#generated-text", {
-    strings: "Hello",
-    autoStart: true,
-    delay: 50,
-  });
+  let userInput = document.querySelector("#search-input");
+  let apiKey = "dd83fab04t350832b43f1o8b985e5044";
+  let prompt = `Please tell me the scientific name, and a short description of this animal ${userInput.value}`;
+  let context = `You a knowledgeable naturalist of animals and can give short, clear answers of no more that 4 lines of text, provided in basic HTML format. The scientific name should be displayed like a title above the rest of the information with a <div> seperating it from the rest of the text,it should be displayed with no capital letters and in bold italic, with the rest of the information below in a paragraph. Also if possible display an emoji of the animal (or closest animal)`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayInfo);
 }
 
 let searchForm = document.querySelector("#search-form");
